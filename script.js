65c6753be2417d780946f7d5328abc879b345540
mdsamir8863 <mentoonsdeveloper.ansari@gmail.com>
2024-06-19 10:33:14 +0530

    goToTop btn added

--- script.js
+++ script.js
@@ -20,7 +20,25 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// go to top:
+// Get the button
+const goToTopBtn = document.getElementById("goToTopBtn");
 
+// Show the button when scrolling down 100px from the top
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    goToTopBtn.classList.remove("hidden");
+    goToTopBtn.classList.add("animate-bounce-in");
+  } else {
+    goToTopBtn.classList.add("hidden");
+    goToTopBtn.classList.remove("animate-bounce-in");
+  }
+};
 
-
-  
+// When the button is clicked, scroll to the top of the document
+goToTopBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
